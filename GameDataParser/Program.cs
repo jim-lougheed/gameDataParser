a15491b241a2c162323b326e5df95b6e6197d7ac
Jim <jims.codes@gmail.com>
2024-11-25 22:45:21 -0500

    Outline initial flow

--- GameDataParser/Program.cs
+++ GameDataParser/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿try
+{
+    var gameDataParserApp = new GameDataParserApp();
+    gameDataParserApp.Run();
+}
+catch (Exception exception)
+{
+    Console.WriteLine(exception.ToString());
+}
+
+public class GameDataParserApp
+{
+    private readonly StringsRepository _stringsRepository;
+    public void Run()
+    {
+        Console.WriteLine("Enter the name of the file you want to read:");
+        bool validFileName = false;
+        while (!validFileName)
+        {
+            var fileName = Console.ReadLine();
+            if (fileName == null)
+            {
+                Console.WriteLine("File name is null");
+            }
+            else if (fileName.Length == 0)
+            {
+                Console.WriteLine("File name is empty.");
+            } 
+            else if (!File.Exists(fileName)) 
+            {
+                Console.WriteLine("File name doesn't exist");
+            } else
+            {
+                validFileName = true;
+                //var videoGameData = _stringsRepository.Read(filePath);
+            }
+        }
+        Console.WriteLine("SUCCESS");
+        Console.ReadKey();
+    }
+}
+
+public class StringsRepository
+{
+
+}
