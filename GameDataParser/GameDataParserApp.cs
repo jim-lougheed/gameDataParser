75700d7b47dbf099bfbe2222bef34f6e52ec5162
Jim <jims.codes@gmail.com>
2024-11-27 17:42:47 -0500

    Create method for file path validation

--- GameDataParser/GameDataParserApp.cs
+++ GameDataParser/GameDataParserApp.cs
@@ -10,51 +10,58 @@
     public void Run()
     {
         bool validFileName = false;
+        string fileName = default;
         while (!validFileName)
         {
             Console.WriteLine("Enter the name of the file you want to read:");
-            var fileName = Console.ReadLine();
-            if (fileName == null)
+            fileName = Console.ReadLine();
+            validFileName = ValidateFilePath(fileName);
+        }
+        try
+        {
+            var gameData = _jsonReader.Read(fileName);
+            if (gameData.Count() > 0)
             {
-                Console.WriteLine("File name cannot be null.");
-            }
-            else if (fileName.Length == 0)
-            {
-                Console.WriteLine("File name cannot be empty.");
-            } 
-            else if (!File.Exists(fileName)) 
-            {
-                Console.WriteLine("File not found.");
-            } else
-            {
-                validFileName = true;
-                try
+                Console.WriteLine("Loaded games are:");
+                foreach (var game in gameData)
                 {
-                    var gameData = _jsonReader.Read(fileName);
-                    if (gameData.Count() > 0)
-                    {
-                        Console.WriteLine("Loaded games are:");
-                        foreach (var game in gameData)
-                        {
-                            Console.WriteLine(game);
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("No games are present in the input file.");
-                    }
-                } catch (JsonParsingException jsonParsingException)
-                {
-                    _logWriter.LogException(jsonParsingException);
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine(jsonParsingException.Message + "JSON body: " + jsonParsingException.JsonBody );
-                    Console.WriteLine("Sorry! The application has experience an unexpected error and will have to be closed.");
-                    Console.ResetColor();
+                    Console.WriteLine(game);
                 }
-                
             }
+            else
+            {
+                Console.WriteLine("No games are present in the input file.");
+            }
+        }
+        catch (JsonParsingException jsonParsingException)
+        {
+            _logWriter.LogException(jsonParsingException);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(jsonParsingException.Message + "JSON body: " + jsonParsingException.JsonBody);
+            Console.WriteLine("Sorry! The application has experience an unexpected error and will have to be closed.");
+            Console.ResetColor();
         }
         Console.WriteLine("Press any key to close.");
         Console.ReadKey();
+    }
+
+    public bool ValidateFilePath(string fileName)
+    {
+        if (fileName == null)
+        {
+            Console.WriteLine("File name cannot be null.");
+            return false;
+        }
+        else if (fileName.Length == 0)
+        {
+            Console.WriteLine("File name cannot be empty.");
+            return false;
+        }
+        else if (!File.Exists(fileName))
+        {
+            Console.WriteLine("File not found.");
+            return false;
+        }
+        return true;
     }
 }
--- GameDataParser/obj/Debug/net8.0/GameDataParser.AssemblyInfo.cs
+++ GameDataParser/obj/Debug/net8.0/GameDataParser.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameDataParser")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23858488fce4c496f2287ba4d6ab48aa880a9d57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7726c9047a026e19dcffe83754975081149c85d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameDataParser")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameDataParser")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
